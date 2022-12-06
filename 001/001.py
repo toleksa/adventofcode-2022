f9c23cb760cd91082bceea85f099ffd262627586
toleksa <toleksa@gmail.com>
2022-12-06 14:15:48 +0200

    init

---
+++ 001/001.py
@@ -0,0 +1,26 @@
+import sys
+
+if sys.argc != 1:
+    print(f'usage: {sys.argv[0]} <file>')
+    exit(1)
+
+FILE=sys.argv[1]
+
+if not os.path.exists(FILE):
+    print(f'ERR: file {FILE} not found')
+    exit(2)
+
+SUM=0
+MAX=0
+with open(FILE) as file:
+    for LINE in file:
+        LINE=line.rstrip()
+        if LINE != "":
+            SUM=SUM+int(LINE)
+        else:
+            if SUM > MAX:
+                MAX = SUM
+            SUM=0
+
+print(MAX)
+
